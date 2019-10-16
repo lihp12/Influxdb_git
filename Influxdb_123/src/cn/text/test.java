94ae47691c54bab5261b20e3de00a6d227c065e6
lihuaping <1807353487@qq.com>
2019-10-16 09:17:01 +0800

    第一次提交

---
+++ Influxdb_123/src/cn/text/test.java
@@ -0,0 +1,17 @@
+package cn.text;
+
+
+public class test {
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		InfluxDBTest infdb=new InfluxDBTest();
+		infdb.setUp();
+		
+		System.out.println(infdb+"连接成功");
+	
+		
+	}
+	
+
+}
